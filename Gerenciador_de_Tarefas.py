3c7e28d9e5f89beec0103152ca4f2ad6a2f2fa85
Matheus Damas <matheusdamasbha@gmail.com>
2023-10-27 20:40:00 -0300

    Update 1.01 Gerenciador_de_tarefas.py
    In this update, I made the hub view visually cleaner for viewing both tasks and information selection. Two lists were also added, completed tasks and pending tasks.

--- Gerenciador_de_Tarefas.py
+++ Gerenciador_de_Tarefas.py
@@ -48,11 +48,39 @@ class TaskManager:
         tasks = cursor.fetchall()
         cursor.close()
 
+        completed_tasks = []
+        pending_tasks = []
+
         for task in tasks:
             task_id, title, description, completed = task
-            # Exibe o numero da tarefa, status, nome e descricao
-            status = 'Status: Concluida' if completed else 'Status: Pendente'
-            print(f'Numero da Tarefa:{task_id}\n{status}\nTarefa: {title}\nDescrição:{description}')
+            if completed:
+                completed_tasks.append(task)
+
+            else:
+                pending_tasks.append(task)
+
+        if completed_tasks:
+            print('Tarefas Concluidas:\n')
+            for task in completed_tasks:
+                task_id, title, description, completed = task
+                status = 'Concluida' if completed else 'Pendente'
+                print(f'''
+Numero da Tarefa: {task_id}
+Status: {status}
+Tarefa: {title.capitalize()}
+Descrição: {description.capitalize()}
+''')
+        if pending_tasks:
+            print('Tarefas Pendentes:\n')
+            for task in pending_tasks:
+                task_id, title, description, completed = task
+                status = 'Concluida' if completed else 'Pendente'
+                print(f'''
+Numero da Tarefa: {task_id}
+Status: {status}
+Tarefa: {title.capitalize()}
+Descrição: {description.capitalize()}
+''')
 
     def update_task(self, task_id, title, description):
         cursor = self.conn.cursor()
@@ -64,7 +92,7 @@ class TaskManager:
         # Deleta uma tarefa com base no seu Id
         cursor.execute('DELETE FROM tasks WHERE id=?', (task_id,))
 
-    def mark_task_as_completed(self, task_id):
+    def mark_task(self, task_id):
         cursor = self.conn.cursor()
         cursor.execute('SELECT completed FROM tasks WHERE id=?', task_id)
         result = cursor.fetchone()
@@ -86,42 +114,42 @@ class TaskManager:
     def run(self):
         while True:
             print('''
-    Gerenciador de Tarefas
+    \n\nGerenciador de Tarefas
     1. Criar Tarefa
     2. Listar Tarefa
     3. Atualizar Tarefa
-    4. Marcar tarefa como concluída
+    4. Marcar ou desmarcar tarefa como concluida
     5. Deletar Tarefa
-    6. Sair''')
+    6. Sair\n''')
 
-            choice = input('Escolha uma opção: ')
+            choice = input('\nEscolha uma opção: ')
 
             if choice == '1':
                 # Opcao para criar uma nova tarefa
-                title = input("Título da tarefa: ")
-                description = input("Descrição da tarefa: ")
+                title = input("\nTítulo da tarefa: ")
+                description = input("\nDescrição da tarefa: ")
                 self.create_task(title, description)
-                print("Tarefa criada com sucesso!")
+                print("\nTarefa criada com sucesso!")
             elif choice == '2':
                 # Opcao para listar as tarefas existentes
-                print('Lista de tarefas')
+                print('\nLista de tarefas\n')
                 self.list_tasks()
             elif choice == '3':
                 # Opcao para atualizar a tarefa selecionada
-                task_id = input('Indice da tarefa a ser atualizada: ')
-                title = input('Novo título da tarefa:')
-                description = input("Nova descrição da tarefa: ")
+                task_id = input('\nIndice da tarefa a ser atualizada: ')
+                title = input('\nNovo título da tarefa:')
+                description = input('\nNova descrição da tarefa: ')
                 self.update_task(task_id, title, description)
-                print('Tarefa atualizada com sucesso')
+                print('\nTarefa atualizada com sucesso')
             elif choice == '4':
                 # Opcao para atualizar a tarefa selecionada para o status 'Concluida'
-                task_id = input("Índice da tarefa concluída: ")
-                self.mark_task_as_completed(task_id)
+                task_id = input("\nÍndice da tarefa concluída: ")
+                self.mark_task(task_id)
             elif choice == '5':
                 # Opcao para deletar a tarefa selecionada
-                task_id = input("Índice da tarefa a ser excluida: ")
+                task_id = input('\nÍndice da tarefa a ser excluida: ')
                 self.delete_task(task_id)
-                print('Tarefa excluida com sucesso!')
+                print('\nTarefa excluida com sucesso!')
             elif choice == '6':
                 # Opcao para sair e salvar 
                 print('Saindo do Gerenciador de Tarefas. Até logo!!')
@@ -134,4 +162,3 @@ class TaskManager:
 if __name__ == '__main__':
     task_manager = TaskManager()
     task_manager.run()
-    task_manager.close_connection()
